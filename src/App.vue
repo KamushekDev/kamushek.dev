7b446b40364f4e1fbe089fb128dcfad3cdda9a83
Ilin Kirill <kamushek.stone@gmail.com>
2024-02-17 05:30:54 +0300

    Changed template to silly site

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,15 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <img alt="My avatar" src="./assets/avatar.png">
+  <UnderDevelopment />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import UnderDevelopment from './components/UnderDevelopment.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    UnderDevelopment
   }
 }
 </script>
---
+++ src/components/UnderDevelopment.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <h1>
+            &lt;разработОЧКА... /&gt;
+        </h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'UnderDevelopment'
+}
+</script>
+
+<style scoped>
+* {
+    margin: 0;
+}
+
+body>div {
+    width: 100vw;
+    height: 100vh;
+    display: table-cell;
+    text-align: center;
+    vertical-align: middle;
+}
+
+h1 {
+    text-align: center;
+    padding: 0;
+}
+</style>
